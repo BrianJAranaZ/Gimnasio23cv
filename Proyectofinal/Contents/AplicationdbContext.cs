420d7db4ab8473346379bab80e0962fb55a15353
lab14sof3 <lab14sof3@DESKTOP-U7ITUHB>
2022-07-08 19:30:15 -0500

    Agregar archivos de proyecto.

---
+++ Proyectofinal/Contents/AplicationdbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyectofinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyectofinal.Contents
+{
+    public class AplicationdbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //HACER UNA CONEXION CON LA BASE DE DATOS CREADA EN XAMPP LLAMADA proyectofin
+            options.UseMySQL("server = localhost; database= proyectofin; user=root; password= ");
+
+        }
+        //add-migration Example
+        //Update-Database
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ Proyectofinal/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Proyectofinal.Contents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyectofinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+
+                string UserResult;
+                string PasswordResult;
+
+                UserResult = txtUser.Text;
+                PasswordResult = txtPassword.Text;
+
+                using (var _context = new AplicationdbContext())
+                {
+                    //Buscamos Primero Usuario, cuando lo encuentra compara el que tienes en el frame y la base de datos
+                    //Comparamos si es igual en la base de datos
+                    //var result = _context.Usuarios.FirstOrDefault(x => x.User == UserResult && x.Password == PasswordResult); 
+                    var result = _context.Usuarios.FirstOrDefault(x => x.User == UserResult && x.Password == PasswordResult);
+                    if(result != null)
+                    {
+
+                        Sistema sistema = new Sistema();
+                        sistema.Show();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("USUARIO & CONTRASEÑA ERRONEOS");
+                    }
+                    
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("!!ERROR INTERNO!! " + ex.Message);
+            }
+
+        }
+    }
+}
